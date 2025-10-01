b9e3b1fbb3d35fd54f806608c34a5ab9023956b3
like <462866046@qq.com>
2025-10-02 07:02:59 +0800

    add fft

--- include/mkl_vec.hpp
+++ include/mkl_vec.hpp
@@ -16,13 +16,25 @@ namespace mkl::vec
     {
         VEC_REPEAT_CODE(T, Sub, n, reinterpret_cast<const mkl_t<T>*>(a), reinterpret_cast<const mkl_t<T>*>(b), reinterpret_cast<mkl_t<T>*>(y));
     }
+    template <typename T> void self_sub(const int n, const T *x, T *y)
+    {
+        sub(n, x, y, y);
+    }
     template<class T> inline void mul(int n, const T*a, const T*b, T* y)
     {
         VEC_REPEAT_CODE(T, Mul, n, reinterpret_cast<const mkl_t<T>*>(a), reinterpret_cast<const mkl_t<T>*>(b), reinterpret_cast<mkl_t<T>*>(y));
     }
+    template <typename T> void self_mul(const int n, const T *x, T *y)
+    {
+        mul(n, x, y, y);
+    }
     template<class T> inline void div(int n, const T*a, const T*b, T* y)
     {
         VEC_REPEAT_CODE(T, Div, n, reinterpret_cast<const mkl_t<T>*>(a), reinterpret_cast<const mkl_t<T>*>(b), reinterpret_cast<mkl_t<T>*>(y));
+    }
+    template <typename T> void self_div(const int n, const T *x, T *y)
+    {
+        div(n, x, y, y);
     }
     template<class T> inline void add(int n, const T a, T* x)
     {
--- src/cpu_backend.cpp
+++ src/cpu_backend.cpp
@@ -1,7 +1,26 @@
 #include <cpu_backend.hpp>
 #include <mkl_vec.hpp>
 #include <mkl_intergral.hpp>
+#include <fftw_fft.hpp>
 
+template<class TFrom, class TTo, int direction = FFTW_FORWARD> void self_fft_impl(TFrom* self, size_t* shape, size_t n)
+{
+    using fft = mekil::fftw<TFrom, TTo>;
+    std::vector<int> row_major_dim(n);
+    for(int i = 0; i < n; i++){
+        row_major_dim.at(i) = shape[n - 1 - i];
+    }
+    fft::transform(fft::make_plan(row_major_dim, direction, self, self).get());
+}
+template<class TFrom, class TTo, int direction = FFTW_FORWARD> void fft_impl(const TFrom* from, TTo* to, size_t* shape, size_t n)
+{
+    using fft = mekil::fftw<TFrom, TTo>;
+    std::vector<int> row_major_dim(n);
+    for(int i = 0; i < n; i++){
+        row_major_dim.at(i) = shape[n - 1 - i];
+    }
+    fft::transform(fft::make_plan(row_major_dim, direction, const_cast<TFrom*>(from), to).get());
+}
 namespace uca
 {
     template<class T> void cpu_backend_impl(cpu_backend<T>& cpu)
@@ -10,10 +29,10 @@ namespace uca
         cpu.VtAdd = mkl::vec::self_add<T>;
         cpu.integral_x = mkl::integral_x<T>;
         cpu.integral_y = mkl::integral_y<T>;
-        // cpu.fft =
-        // cpu.fft_outplace =
-        // cpu.ifft =
-        // cpu.ifft_outplace =
+        cpu.self_fft = self_fft_impl<T, complex_t<T>, FFTW_FORWARD>;
+        cpu.self_ifft = self_fft_impl<complex_t<T>, T, FFTW_BACKWARD>;
+        cpu.fft = fft_impl<T, complex_t<T>, FFTW_FORWARD>;
+        cpu.ifft = fft_impl<complex_t<T>, T, FFTW_BACKWARD>;
     }
     template<> cpu_backend<float>::cpu_backend(){cpu_backend_impl(*this);}
     template<> cpu_backend<double>::cpu_backend(){cpu_backend_impl(*this);}
